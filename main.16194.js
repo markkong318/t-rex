69c3cb30a67adfdafd34cbf10c8f9845b33a112e
mark <mark@rgames.jp>
2020-06-12 18:18:14 +0900

    Updates

--- main.16194.js
+++ main.16194.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ebcac.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.db868.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
