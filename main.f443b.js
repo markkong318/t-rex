464ac13256c8b36388c88a0ff2eb69cb021f9cab
mark <mark@rgames.jp>
2020-06-15 14:26:37 +0900

    Updates

--- main.f443b.js
+++ main.f443b.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6253d.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.6ce46.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
